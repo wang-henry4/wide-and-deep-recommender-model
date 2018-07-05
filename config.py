d140fe12094bbc4a1577d8fb208aa041ea376bcd
Henry Wang <40856056+wang-henry4@users.noreply.github.com>
2018-07-05 18:18:19 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,88 @@
+# names of the columns of the input dataset, in this order
+COLUMN_NAMES = [
+    'event_id',
+    'tenant_id',
+    'user_agent',
+    'operation_type',
+    'operation_result',
+    'operation_description',
+    'source_ip',
+    'app_id',
+    'uid',
+    'day',
+    'hour',
+    'minute',
+    'second',
+    'risk_label',
+    'weight_column'
+]
+
+# datatypes of the column in the order of COLUMN_NAMES
+COLUMN_DTYPES = [
+    'str',
+    'str',
+    'str',
+    'str',
+    'str',
+    'str',
+    'str',
+    'str',
+    'str',
+    'int',
+    'int',
+    'int',
+    'int',
+    'int',
+    'int',
+]
+
+# the categorical features
+CATEGORICAL_FEATURES = [
+    'event_id',
+    'tenant_id',
+    'user_agent',
+    'operation_type',
+    'operation_result',
+    'operation_description',
+    'source_ip',
+    'app_id',
+    'uid'
+]
+
+# the continuous features
+CONTINUOUS_FEATURES = [
+    'day',
+    'hour',
+    'minute',
+    'second'
+]
+
+WEIGHT_COLUMN = 'weight_column'
+
+LABEL = 'risk_label' 
+
+HIDDEN_LAYERS = [256, 128, 64]
+
+EMBEDDING_DIMENSIONS = {
+    'event_id': 10,
+    'tenant_id': 10,
+    'user_agent': 10,
+    'operation_type': 10,
+    'operation_result': 10,
+    'operation_description': 10,
+    'source_ip': 10,
+    'app_id': 10,
+    'uid': 10
+}
+
+DROPOUT = 0
+
+TRAIN_PATH = '' # What is the path
+
+TEST_PATH = '' # what is the test path
+
+WITH_WEIGHT = False
+
+EPOCHS = 1
+
+VERBOSE = True
